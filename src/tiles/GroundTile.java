940f42e3e303bc7b22b0f355addea4100ec64eef
justinkim <yulli@yulli.org>
2014-02-02 22:04:36 -0500

    Add some Tiles

---
+++ src/tiles/GroundTile.java
@@ -0,0 +1,10 @@
+package NetworkedOgre.tiles;
+
+import NetworkedOgre.units.Unit;
+
+public class GroundTile extends Tile {
+
+    public GroundTile() {
+        super();
+    }
+}
---
+++ src/tiles/NothingTile.java
@@ -0,0 +1,10 @@
+package NetworkedOgre.tiles;
+
+import NetworkedOgre.units.Unit;
+
+public class NothingTile extends Tile {
+
+    public NothingTile() {
+        super();
+    }
+}
