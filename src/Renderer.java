8fb80e493e1734f9767e376704a96cd70a020636
Zane Sterling <sterling.zane@gmail.com>
2014-08-10 21:03:30 -0400

    Make the Renderer display a window

--- src/Renderer.java
+++ src/Renderer.java
@@ -1,13 +1,28 @@
 package NetworkedOgre;
 
+import javax.swing.JFrame;
+import java.awt.Dimension;
+
 public class Renderer {
 
-	Game game;
+	private static final int WIDTH = 640;
+	private static final int HEIGHT = 480;
+
+	private Game game;
+	private JFrame frame;
 
 	public Renderer(Game game) {
 		game = game;
+
+		// initialize the frame
+		frame = new JFrame("Networked Ogre");
+		frame.setMinimumSize(new Dimension(640, 480));
+		frame.setMaximumSize(new Dimension(640, 480));
+		frame.pack();
+		frame.setVisible(true);
 	}
 
+	// render the board to the frame and display it
 	public void render() {
 		
 	}
