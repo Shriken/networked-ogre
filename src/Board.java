7a753025782b4da5af1b9348180d6737427a1e3d
Justin Kim <yulli@yulli.org>
2014-08-10 21:15:24 -0400

    Fix silly things

--- src/Board.java
+++ src/Board.java
@@ -2,11 +2,11 @@ package NetworkedOgre;
 
 public class Board  {
     // Size of columns 1, 3, 5, ...
-    private static final ODD_COLUMN_SIZE = 21;
+    private static final int ODD_COLUMN_SIZE = 21;
     // Size of columns 2, 4, 6, ...
-    private static final EVEN_COLUMN_SIZE = 20;
+    private static final int EVEN_COLUMN_SIZE = 20;
     // Number of total columns
-    private static final WIDTH = 15;
+    private static final int WIDTH = 15;
 
     private Tile[][] tilemap;
 
@@ -16,8 +16,8 @@ public class Board  {
     }
 
     public void reset() {
-        for (int i = 0; i < tilemap.length, i++) {
-            for (int j = 0; j < tilemap[0].length, j++) {
+        for (int i = 0; i < tilemap.length; i++) {
+            for (int j = 0; j < tilemap[0].length; j++) {
                 setTile(i, j, new ClearTile());
             }
         }
