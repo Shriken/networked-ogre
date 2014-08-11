69b236157e2a8625020e42da62087390771c23bb
Zane Sterling <sterling.zane@gmail.com>
2014-08-10 20:53:52 -0400

    Add game structure

---
+++ src/Game.java
@@ -0,0 +1,6 @@
+package NetworkedOgre;
+
+public class Game {
+	public Game() {}
+	public void tick() {}
+}
---
+++ src/Renderer.java
@@ -0,0 +1,14 @@
+package NetworkedOgre;
+
+public class Renderer {
+
+	Game game;
+
+	public Renderer(Game game) {
+		game = game;
+	}
+
+	public void render() {
+		
+	}
+}
