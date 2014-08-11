debcee215eab889d41f9d50ae3614f95f5089114
Justin Kim <yulli@yulli.org>
2014-08-10 22:02:35 -0400

    Make CraterTiles unable to accept units

--- src/tiles/CraterTile.java
+++ src/tiles/CraterTile.java
@@ -2,8 +2,20 @@ package NetworkedOgre.tiles;
 
 import java.awt.Color;
 
+import NetworkedOgre.units.Unit;
+
 public class CraterTile extends Tile {
     public CraterTile() {
         super(Color.BLACK);
+    }
+
+    public void addUnit(Unit unit) {
+        throw new NoPutTheUnitInTheCraterException();
+    }
+}
+
+public class NoPutTheUnitInTheCraterException extends Exception {
+    public NoPutTheUnitInTheCraterException(String message) {
+        super(message);
     }
 }
