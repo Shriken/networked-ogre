0ec3d39c942fc52aa3c697763c6e7dd18000caef
Justin Kim <yulli@yulli.org>
2014-08-10 22:17:01 -0400

    Add ridges to Tiles

--- src/tiles/Tile.java
+++ src/tiles/Tile.java
@@ -7,10 +7,12 @@ import NetworkedOgre.units.Unit;
 
 public class Tile {
     public Color color;
+    public boolean[] ridges;
     public ArrayList<Unit> units;
 
     public Tile(Color color) {
         this.color = color;
+        this.ridges = new boolean[];
         this.units = new ArrayList<Unit>();
     }
 
@@ -20,5 +22,21 @@ public class Tile {
 
     public ArrayList<Unit> getUnits() {
         return units;
+    }
+
+    /* Ridges of a tile are represented by an array of length 6.
+     * Indices represent the ridge in a clockwise direction, with 0 being the
+     * top-most ridge.
+     */
+    public boolean getRidge(int position) {
+        return ridges[position];
+    }
+
+    public void addRidge(int position) {
+        ridges[position] = true;
+    }
+
+    public void removeRidge(int position) {
+        ridges[position] = false;
     }
 }
